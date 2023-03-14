8f5e5218a6c9dd89511098e3305a74c9a23c74c5
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-03-14 21:46:59 +0200

    adds owner

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,8 @@ const { Contact } = require('../models/contact');
 const { HttpError, ctrlWrapper } = require('../helpers')
 
 const listContacts = async (req, res) => {
-    const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner }, "-createdAT -updateAt");
     res.json(result);
 }
 
@@ -18,7 +19,8 @@ const getContactById = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-    const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+    const result = await Contact.create({...req.body, owner});
     res.status(201).json(result);
 }
 
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -15,10 +15,10 @@ const authenticate = async (req, res, next) => {
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         const  user  = await User.findById(id);
-
         if (!user) {
             next(HttpError(401));
         }
+        req.user = user;
         next();
     }
     catch {
